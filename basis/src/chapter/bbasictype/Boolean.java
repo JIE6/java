8ec703376ed37a84c2b16a9664559ac2d06effe5
JIE <37128250+JIE6@users.noreply.github.com>
2019-08-28 23:09:43 +0800

    Update Boolean.java

--- basis/src/chapter/bbasictype/Boolean.java
+++ basis/src/chapter/bbasictype/Boolean.java
@@ -1,6 +1,7 @@
 package chapter.bbasictype;
 
 /**
+ * 重点
  * 布尔型只有一个boolean 类型，用于表示逻辑上的"真"或"假"。
  * 在Java 语言中， boolean 类型的数值只能是true 或false ，不能用。或者非0 来代表。其他基本数据类型的值也不能转换成boolean 类型。
  * boolean 类型的值或变量主要用做旗标来进行流程控制， Java 语言中使用boolean 类型的变量或值控制的流程主要有如下几种。
