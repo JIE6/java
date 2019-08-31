5b6afcd7055d2b3bedfeeca118eaa6daa5e630c9
JIE-Lenovo <1526430265@qq.com>
2019-08-31 18:49:22 +0800

    八大基本类型回顾1.4 运算符-位运算符(未完成)

---
+++ basis/src/basictype/OperatorAssignment.java
@@ -0,0 +1,43 @@
+package basictype;
+
+/**
+ * 重点
+ * 运算符-赋值运算符
+ * 赋值运算符用于为变量指定变量值，与C类似， Java 也使用=作为赋值运算符。通常，使用赋值运算符将一个直接量值赋给变量。例如如下代码。
+ * @author JIE
+ */
+public class OperatorAssignment {
+
+    public static void main(String[] args) {
+        // 为变量str 赋值Java
+        String str = " Java";
+        //为变量pi赋值3.14
+        double pi = 3.14;
+        //为变量visited 赋值true
+        boolean visited = true;
+        // 除此之外，也可使用赋值运算符将一个变量的值赋给另一个变量
+        // 将变量str 的值赋给str2
+        String str2 = str;
+        System.out.println(str);
+        System.out.println(pi);
+        System.out.println(visited);
+        System.out.println(str2);
+        /**
+         * 按前面关于交量的介绍，可以把交量当成一个可盛装数据的容器。而赋值运算就是将;
+         * 被赋的值"装入"变量的过程。赋值运算符是从右向左执行计算的，
+         * 程序先计算得到=右边的值，然后将该值"装入" =左边的变量，因此赋值运算符(=)左边只能是变量
+         * ------------------------------------------------------------------------------------------------
+         * 值得指出的是，赋值表达式是有值的，赋值表达式的值就是右边被赋的值。例如String str2 = 附表达式的值就是str 。
+         * 因此，赋值运算符支持连续赋值，通过使用多个赋值运算符，可以一次为多个变量赋值。如下代码
+         */
+        int a;
+        int b;
+        int c;
+        // 通过为a ， b ， c 赋值， 三个变量的值都是7
+        // 虽然Java 支持这种一次为多个变量赋值的写法，但这种写法导致程序的可读性降低，因此不推荐这样写。
+        a=b=c=7;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
